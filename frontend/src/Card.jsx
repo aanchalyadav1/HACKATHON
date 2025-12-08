93ada491a9c8178c4115c6cba419243d7fdcdeb9
Aanchal Yadav <yadavaanchal2005@gmail.com>
2025-12-08 20:06:56 +0530

    Delete frontend/src/Card.jsx

--- frontend/src/Card.jsx
+++
@@ -1,7 +0,0 @@
-export default function Card({children,className=''}) {
-  return (
-    <div className={'p-5 rounded-xl bg-white/5 border border-white/10 backdrop-blur ' + className}>
-      {children}
-    </div>
-  );
-}
