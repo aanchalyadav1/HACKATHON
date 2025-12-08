6fbfb3b2475efa222ed6617276f72963112f727c
Aanchal Yadav <yadavaanchal2005@gmail.com>
2025-12-08 20:08:18 +0530

    Delete frontend/src/Toast.jsx

--- frontend/src/Toast.jsx
+++
@@ -1,7 +0,0 @@
-export default function Toast({message}) {
-  return (
-    <div className='fixed bottom-5 right-5 bg-blue-600 text-white px-4 py-2 rounded shadow-lg animate-pulse'>
-      {message}
-    </div>
-  );
-}
